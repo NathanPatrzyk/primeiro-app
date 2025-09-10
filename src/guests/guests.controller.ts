a2a5881dd8eba6dc29e67153db748e1f70478894
Nathan Patrzyk <nathanpatzyk11@gmail.com>
2025-09-10 20:15:11 -0300

    feat: refactor GuestsController and GuestsService to improve guest management functionality

--- src/guests/guests.controller.ts
+++ src/guests/guests.controller.ts
@@ -1,17 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { GuestsService } from './guests.service';
+import { CreateGuestDto } from './dto/create-guest.dto';
 
 @Controller('guests')
 export class GuestsController {
   constructor(private readonly guestsService: GuestsService) {}
 
   @Get()
-  findAllGuests() {
-    return this.guestsService.findAllGuests();
+  findAll() {
+    return this.guestsService.findAll();
   }
 
   @Get(':id')
-  findOneGuests() {
-    return this.guestsService.findOneGuests();
+  find(@Param('id') id: string) {
+    return this.guestsService.find(id);
+  }
+
+  @Post()
+  create(@Body() createGuestDto: CreateGuestDto) {
+    return this.guestsService.create(createGuestDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateGuestDto: CreateGuestDto) {
+    return this.guestsService.update(id, updateGuestDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.guestsService.delete(id);
   }
 }
--- src/guests/guests.service.ts
+++ src/guests/guests.service.ts
@@ -1,25 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateGuestDto } from './dto/create-guest.dto';
+import { UpdateGuestDto } from './dto/update-guest.dto';
 
 @Injectable()
 export class GuestsService {
-  findAllGuests() {
-    return [
-      {
-        id: 1,
-        guest: 'José',
-      },
-      {
-        id: 2,
-        guest: 'João',
-      },
-      {
-        id: 3,
-        guest: 'Maria',
-      },
-    ];
+  private guests = [
+    {
+      id: 1,
+      name: 'Nome do Convidado',
+    },
+  ];
+
+  findAll() {
+    return this.guests;
   }
 
-  findOneGuests() {
-    return 'Retornando um único convidado';
+  find(id: string) {
+    const guest = this.guests.find((guest) => guest.id === Number(id));
+
+    if (guest) return guest;
+
+    throw new HttpException('Esse Convidado Não Existe!', HttpStatus.NOT_FOUND);
+  }
+
+  create(createGuestDto: CreateGuestDto) {
+    const newId = this.guests.length + 1;
+
+    const newGuest = {
+      id: newId,
+      ...createGuestDto,
+    };
+
+    this.guests.push(newGuest);
+
+    return newGuest;
+  }
+
+  update(id: string, updateGuestDto: UpdateGuestDto) {
+    const guestIndex = this.guests.findIndex(
+      (guest) => guest.id === Number(id),
+    );
+
+    if (guestIndex < 0) {
+      throw new HttpException(
+        'Esse Convidado Não Existe!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const guestItem = this.guests[guestIndex];
+
+    this.guests[guestIndex] = {
+      ...guestItem,
+      ...updateGuestDto,
+    };
+
+    return this.guests[guestIndex];
+  }
+
+  delete(id: string) {
+    const guestIndex = this.guests.findIndex(
+      (guest) => guest.id === Number(id),
+    );
+
+    if (guestIndex < 0) {
+      throw new HttpException(
+        'Esse Convidado Não Existe!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    this.guests.splice(guestIndex, 1);
+
+    return 'Convidado Excluído com Sucesso!';
   }
 }
